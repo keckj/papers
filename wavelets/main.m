59eadcae29f33a9df710186f1b5224727f9beaba
Your Name <you@example.com>
2015-01-04 16:39:25 +0100

    main

--- wavelets/main.m
+++ wavelets/main.m
@@ -1,11 +1,12 @@
 
 function main() 
-    N = 1024;c
+    N = 64;
     xmin = -3.14/2;
     xmax = 3.14/2;
     
     [x,f] = sample(N, xmin, xmax, @sin);
-    plot(x,f);
+    %plot(x,f,'x');
+    DB(0,0,0,1,10);
 end
 
 
@@ -13,5 +14,21 @@ function [x,f] = sample(n, xmin, xmax, func)
     x = xmin + rand(1,n)*(xmax - xmin);
     x = sort(x);
     f = arrayfun(func, x);
+end
+
+function y = phi(x) 
+    if(abs(x) > 1)
+        y = 0;
+    else 
+        y = 1;
+    end
+end
+
+function phi_jk = DB(j,k,xmin,xmax,pts)
+    dx = (xmax - xmin)/(pts-1);
+    for x = xmin:dx:xmax
+        phi(2^-j * x + k);
+    end
+    
 end
 
